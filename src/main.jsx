6f8315f210fc05bc9b6d0dde365d6478f955ab38
ianrebulado <eilrebulado@gmail.com>
2023-11-08 20:21:49 +0800

    updated structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './styles/index.css'
+import LoginPage from './pages/LoginPage/LoginPage'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LoginPage />
   </React.StrictMode>,
 )
