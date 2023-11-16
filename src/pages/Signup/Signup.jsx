9d9f0c3ede6f0a0f55241fcb937cd21d0337a247
ianrebulado <eilrebulado@gmail.com>
2023-11-16 23:35:24 +0800

    feat: add routes, fix css, finish signup page design

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -3,21 +3,22 @@ import FormLabel from '../../components/FormLabel'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 import '../Signup/signup.css'
+import { Link } from 'react-router-dom'
 
 export default function Signup() {
   return (
     <div className="position">
-        <div className="container">
+        <div className="signup-container">
+            <form className='signupform-container'>
             <h3>Create an account</h3>
-            <form className='form-container'>
               <FormLabel label={'EMAIL'} />
-              <Input />
+              <Input className={'input-field'} />
               <FormLabel label={'PASSWORD'} />
-              <Input />
+              <Input className={'input-field'} />
               <FormLabel label={'CONFIRM PASSWORD'} />
-              <Input />
-            <Button />
-              
+              <Input className={'input-field'} />
+            <Button className={'login-btn'} text={'Continue'}  />
+            <Link to={'/'}> Already have an account? </Link>
             </form>
         </div>
     </div>
