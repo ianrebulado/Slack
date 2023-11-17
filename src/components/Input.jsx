43a0672863d88c1b044247604598385dba1bfc7f
ianrebulado <eilrebulado@gmail.com>
2023-11-17 11:46:04 +0800

    refactor: input field and state

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Input({className, placeholder, type, onChange}) {
+export default function Input({className, placeholder, type, onChange, value, name}) {
   return (
-    <input className={className} placeholder={placeholder} type={type} onChange={onChange} autoComplete='off'/>
+    <input className={className} placeholder={placeholder} type={type} name={name} onChange={onChange} autoComplete='off' value={value}/>
   )
 }
