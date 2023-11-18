5a2bd2b8f82df2de037ca93c4c05bbe574232830
ianrebulado <eilrebulado@gmail.com>
2023-11-18 18:09:07 +0800

    chore: add react select, some styling

--- src/components/Sidebar/Modal/ServerModal.jsx
+++ src/components/Sidebar/Modal/ServerModal.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Input from '../../Input'
 import FormLabel from '../../FormLabel'
 import Button from '../../Button'
+import Select from 'react-select'
 import '../Modal/modal.css'
 
 export default function Modal({ onClose }) {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('http://206.189.91.54/api/v1/users', {
+          method: 'GET',
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+  
+
+  const options = [
+    { value: 'hehe' , label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
+
+
+
+
+
+
+  
     return (
       <div className="modal-container">
         <div className="modal">
@@ -12,6 +46,7 @@ export default function Modal({ onClose }) {
           <FormLabel label={'SERVER NAME'} />
           <Input className={'server-input'}/>
           <FormLabel label={'SELECT USERS'} />
+          <Select options={options}/>
           
           <Button className={'create-btn'} text={'CREATE'} />
           <Button className={'close-btn'} handleClick={onClose} text={'CLOSE'} />
