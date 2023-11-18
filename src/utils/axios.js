ec12f90a11a2252b9072cbdb02540e1880dd4ec8
ianrebulado <eilrebulado@gmail.com>
2023-11-19 00:25:12 +0800

    chore: add comments

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -10,3 +10,20 @@ export const Slack = axios.create({
         'client': localStorage.getItem("client") || '',
     }
 })
+
+// const res = await Slack.post('/auth/sign_in', payload)
+    
+    // const token = res.headers.get('access-token');
+    // const uid = res.headers.get('uid');
+    // const expiry = res.headers.get('expiry');
+    // const client = res.headers.get('client');
+
+    // Slack.defaults.headers['access-token'] = token;
+    // Slack.defaults.headers['uid'] = uid;
+    // Slack.defaults.headers['expiry'] = expiry;
+    // Slack.defaults.headers['client'] = client;
+
+    // localStorage.setItem('token', token)
+    // localStorage.setItem('uid', uid)
+    // localStorage.setItem('expiry', expiry)
+    // localStorage.setItem('client', token)
