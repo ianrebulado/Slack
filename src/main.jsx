0adf81ae1331a211d8df790a4ddeab8b1267928a
ianrebulado <eilrebulado@gmail.com>
2023-11-18 10:22:28 +0800

    chore: remove test page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import Testpage from "./pages/Test/testpage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Signup from "./pages/Signup/Signup";
 import Home from './pages/Home/Home'
