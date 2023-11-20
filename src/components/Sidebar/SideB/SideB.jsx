cf75b84f943eea12f32940a0ad80844dccd3f5f7
ianrebulado <eilrebulado@gmail.com>
2023-11-20 15:14:43 +0800

    chore: add styles

--- src/components/Sidebar/SideB/SideB.jsx
+++ src/components/Sidebar/SideB/SideB.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import ChatWindow from '../../Chat/ChatWindow'
+
+
+import { Users2, Rocket, User2, Settings, Mic, Headphones, Plus } from 'lucide-react'
 import '../SideB/side-b.css'
-import { Users2, Rocket, User2, Settings, Mic, Headphones } from 'lucide-react'
 
 export default function SideB() {
 
@@ -12,10 +15,31 @@ export default function SideB() {
       <div className='top-separator' />
       <div className='sideb-header'> <Users2 strokeWidth={2.5}/> Friends </div>
       <div className='sideb-header'> <Rocket strokeWidth={2.5} /> Turbo </div>
-      <div className='user'> 
+
+      <div className='message-header'> <div> Messages </div> <Plus size={16} strokeWidth={2}/> </div>
+    <section className='messages'>
+    <ChatWindow /> 
+    <ChatWindow />
+    <ChatWindow />
+    <ChatWindow />
+    <ChatWindow /> 
+    <ChatWindow />
+    <ChatWindow />
+    <ChatWindow />
+    <ChatWindow /> 
+    <ChatWindow />
+    <ChatWindow />
+    <ChatWindow />
+    <ChatWindow /> 
+    <ChatWindow />
+    <ChatWindow />
+    <ChatWindow />
+    </section>
+
+      <section className='user'> 
       <div className='username'> <User2 size={18}/> { username }</div>  
       <Mic size={17} /> <Headphones size={17}/> <Settings size={17} />
-       </div>
+       </section>
     </div>
   )
 }
