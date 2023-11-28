6323fbf39e72e809fb728c3e51d1458c52de3e4d
ianrebulado <eilrebulado@gmail.com>
2023-11-29 06:56:50 +0800

    feat: add member to server

---
+++ src/components/Chat/AddMember.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Slack } from "../../utils/axios";
+import FormLabel from "../FormLabel";
+import Button from "../Button";
+import Select from "react-select";
+import { toastSuccess } from "../../utils/toast";
+
+export default function AddMember({ channelID, onClose, onModalSubmit }) {
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  async function fetchUsers() {
+    try {
+      const res = await Slack.get("/users");
+      console.log(res);
+      const usersData = Object.values(res.data.data).flat();
+      setUsers(usersData);
+    } catch (error) {}
+  }
+
+  if (users.length === 0) {
+    fetchUsers();
+  }
+
+  const options = users.map((user) => ({
+    value: user.id,
+    label: user.uid,
+  }));
+
+  function addUser(userOption) {
+    if (userOption) {
+      setSelectedUser(userOption.value);
+    }
+  }
+
+  const addPayload = {
+    id: channelID,
+    member_id: selectedUser,
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onModalSubmit(addPayload)
+    
+  }
+
+  return (
+    <div className="modal-container">
+      <form className="modal" onSubmit={handleSubmit}>
+        <h2> Add Members </h2>
+        <FormLabel label={"SELECT USER"} />
+        <Select
+          options={options}
+          placeholder=""
+          className="select-dropdown"
+          onChange={addUser}
+        />
+        <div className="buttons">
+          <Button
+            className={"close-btn"}
+            handleClick={onClose}
+            text={"CLOSE"}
+          />
+          <Button
+            className={"create-btn"}
+            text={"CREATE"}
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/components/Chat/ChatWindow.jsx
+++ src/components/Chat/ChatWindow.jsx
@@ -3,15 +3,18 @@ import Input from "../Input";
 import { useLoaderData } from "react-router-dom";
 import "../Chat/chatwindow.css";
 import { Slack } from "../../utils/axios";
-import { User } from "lucide-react";
+import { User, PlusCircle } from "lucide-react";
 import Loader from "../../pages/Loader/Loader";
+import AddMember from "./AddMember";
+import { toastError, toastSuccess } from "../../utils/toast";
 
-export default function ChatWindow({}) {
+export default function ChatWindow({ fetchUsers }) {
   const [conversationData, setConversationData] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [addModal, setAddModal] = useState(false);
   const channelID = useLoaderData();
-  const chat = useRef(null)
+  const chat = useRef(null);
 
   const payload = {
     receiver_id: channelID,
@@ -38,42 +41,59 @@ export default function ChatWindow({}) {
           senderUid: msgs.sender.uid,
         }));
 
-        if(mappedData.length === conversationData.length) {
-          return null 
+        if (mappedData.length === conversationData.length) {
+          return null;
         } else {
-        setConversationData(mappedData)};
-        setLoading(false)
+          setConversationData(mappedData);
+        }
+        setLoading(false);
       } catch (error) {
         console.log(error);
-      } 
+      }
     }
-    const interval = setInterval(fetchMsgs, 500)
-    
+    const interval = setInterval(fetchMsgs, 500);
+
     return () => clearInterval(interval);
   }, [channelID]);
-
-
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if (inputValue.trim() === '') {
+    if (inputValue.trim() === "") {
       return;
     }
-
     try {
       const res = await Slack.post("/messages", reqBody);
     } catch (error) {
-      console.log(error)
-    } finally{
-      setInputValue('')
+      console.log(error);
+    } finally {
+      setInputValue("");
     }
   }
 
   function handleChange(e) {
-    setInputValue(e.target.value)
+    setInputValue(e.target.value);
   }
 
+  function handleClick() {
+    setAddModal(true);
+  }
+
+function addMember(addPayload){
+  async function onModalSubmit() {
+    console.log("clicked");
+    console.log(addPayload);
+    try {
+      const res = await Slack.post("channel/add_member", addPayload);
+      toastSuccess("Added new member");
+    } catch (error) {
+      toastError("Invalid");
+    } finally {
+      setAddModal(false);
+    }
+  }
+  onModalSubmit()
+}
 
   useEffect(() => {
     chat.current.scrollTop = chat.current.scrollHeight;
@@ -81,35 +101,52 @@ export default function ChatWindow({}) {
 
   return (
     <div className="chat-window">
-      <div className="chat-header">{channelID}</div>
+      <div className="chat-header">
+        {channelID}{" "}
+        <PlusCircle size={20} cursor={"pointer"} onClick={handleClick} />{" "}
+      </div>
       <div className="window-content" ref={chat}>
+        {!loading ? (
+          conversationData.map((msg, index) => (
+            <Chat key={index} sender={msg.senderUid} msg={msg.body} />
+          ))
+        ) : (
+          <Loader />
+        )}
 
-      
-        {!loading ? conversationData.map((msg, index) => (
-          <Chat key={index} sender={msg.senderUid} msg={msg.body} />
-        )) : <Loader />}
-  
-
-
-
+        {addModal && (
+          <AddMember
+            channelID={channelID}
+            onModalSubmit={addMember}
+            onClose={() => setAddModal(false)}
+          />
+        )}
       </div>
       <form onSubmit={handleSubmit} className="msg-form">
-        <Input placeholder={"Message"} className={"msg-input"} type={"text"}
-        onChange={handleChange} value={inputValue}/>
+        <Input
+          placeholder={"Message"}
+          className={"msg-input"}
+          type={"text"}
+          onChange={handleChange}
+          value={inputValue}
+        />
       </form>
     </div>
   );
 }
 
 function Chat({ sender, msg }) {
-  const you = localStorage.getItem('uid')
+  const you = localStorage.getItem("uid");
   return (
     <div className="msgs">
-     <div className="icon"> <User/></div>
-     <div className="msg-content">
-     <div className="sender"> {sender === you ? "You" : sender } </div>
-     <div className="message"> {msg} </div>
-     </div>
+      <div className="icon">
+        {" "}
+        <User />
+      </div>
+      <div className="msg-content">
+        <div className="sender"> {sender === you ? "You" : sender} </div>
+        <div className="message"> {msg} </div>
+      </div>
     </div>
   );
 }
